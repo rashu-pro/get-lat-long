d889ff9c3fc5b68e8cf80afa1a72c179425a0e86
rashu-pro <rashu.pro@gmail.com>
2023-09-23 16:54:53 +0600

    address verified for new list

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const fs = require('fs'); // Include the Node.js file system module
+
 const googleMapsClient = require('@google/maps').createClient({
   key: 'AIzaSyCRIIew-eQp2QjI5mRLFOE-qoUnl-qKC38' // Replace with your actual Google Maps API key
 });
+
+async function getAddress(address, supportingAddressParameter, count) {
+  let parameter = supportingAddressParameter[count];
+  let addressQuery = address + ',' + parameter;
+  let formattedAddress = await geocodeAddress(addressQuery);
+  if (formattedAddress.length < 1) {
+    count = count + 1;
+    getAddress(address, supportingAddressParameter, count);
+    return;
+  }
+
+  console.log('address: ', address);
+  console.log('formatted address: ', formattedAddress[0].formatted_address);
+  console.log('----------------');
+  // console.log('formatted address: ', formattedAddress);
+  return formattedAddress;
+}
+
+// let supportingAddressParameter = [1949, 'Plano', 'Texas'];
+// getAddress('4700 14th Street', supportingAddressParameter, 0);
+// return;
 
 // Function to geocode addresses and update the array
 function geocodeAddressesAndSaveToJson(data) {
@@ -9,12 +31,25 @@ function geocodeAddressesAndSaveToJson(data) {
     const address = `${school["SCHOOL_ADDRESS"]}, ${school["ZIP_CODE"]}`;
 
     // Geocode the address
-    const response = await geocodeAddress(address);
+    // const response = await geocodeAddress(address);
+    let zipCode = String(school['ZIP_CODE']);
+    if(zipCode.length<5){
+      zipCode = '0'+zipCode;
+      // zipCode = parseInt(zipCode);
+      console.log(zipCode);
+      console.log('============');
+    }
+    const addressParameter = [zipCode, school['CITY'], school['STATE']];
+    const response = await getAddress(school['SCHOOL_ADDRESS'], addressParameter, 0);
+
+    // console.log('address: ', response[0].address_components);
+    // console.log(response[0].formatted_address);
 
     if (response && response[0]) {
       // Update latitude and longitude in the original data
       school["LATITUDE"] = response[0].geometry.location.lat;
       school["LONGITUDE"] = response[0].geometry.location.lng;
+      school["SCHOOL_ADDRESS"] = response[0].formatted_address.replace(/, USA$/, '');
     }
 
     return school;
@@ -58,7 +93,7 @@ function saveToJsonFile(data, fileName) {
 }
 
 // Call the function to start geocoding and saving to JSON
-fs.readFile('islamic-school-directory-json.json', 'utf8', (err, data) => {
+fs.readFile('isla-school-directory.json', 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading the input JSON file:', err);
     return;
